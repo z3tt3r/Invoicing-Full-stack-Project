83530bde0648a490afe4d1380cbabd92797f4a71
Michal Musil <mi.musil@gmail.com>
2025-10-22 20:50:39 +0200

    Add SmokeTest for Spring Boot application

---
+++ invoice-server-starter/src/test/java/cz/itnetwork/SmokeTest.java
@@ -0,0 +1,12 @@
+package cz.itnetwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmokeTest {
+    @Test
+    void contextLoads() {
+        // If the Spring context fails to load, this test will fail.
+    }
+}
